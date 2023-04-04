32de96e4cffa002cc6a0d838b3ef5628159f34d1
Keith Smiley <keithbsmiley@gmail.com>
2023-04-04 15:48:05 -0700

    Add support for demangling other USR types
    Usrs sometimes have multiple leading identifiers, in this case `e:` for
    extension. Previously we supported stripping a single `s:`, now we
    support stripping all the leading identifiers as long as they still
    start with `s:` (and not `c:` like some Objective-C USRs).

--- Tests/SwiftDemangleTests/SwiftDemangleTests.swift
+++ Tests/SwiftDemangleTests/SwiftDemangleTests.swift
@@ -1,12 +1,14 @@
 import XCTest
 import SwiftDemangle
 
+private let kCommonSymbol = "s:10DriverCore28AddPhoneVerifyViewControllerC28resendCodeViaVoiceCallButton33_3079D27A166598D3B3B79EAC945873F9LLSo8UIButtonCSgvp"
+private let kExtensionUSR = "s:e:s:14Unidirectional14EffectProducerV10Onboarding7LyftKit13NonemptyStackVyAD0D5StateV_yAD9GuestUserOctGRszAD0D10ActionMask_pRs_rlE010submitNameB0ACyAldM_pGvpZ"
+
 final class SwiftDemangleTests: XCTestCase {
-    private static let CommonSymbol = "s:10DriverCore28AddPhoneVerifyViewControllerC28resendCodeViaVoiceCallButton33_3079D27A166598D3B3B79EAC945873F9LLSo8UIButtonCSgvp"
 
     func testDemangle() throws {
         let demangler = Demangler()
-        let root = try XCTUnwrap(demangler.demangle(symbol: Self.CommonSymbol))
+        let root = try XCTUnwrap(demangler.demangle(symbol: kCommonSymbol))
         let moduleNode = root.children[0].children[0].children[0]
         try XCTAssertEqual(XCTUnwrap(moduleNode.text), "DriverCore")
         let classNode = root.children[0].children[0].children[1]
@@ -19,9 +21,16 @@ final class SwiftDemangleTests: XCTestCase {
         XCTAssertNil(demangler.demangle(symbol: symbol))
     }
 
+    func testSwiftExtensionUSR() throws {
+        let demangler = Demangler()
+        let root = try XCTUnwrap(demangler.demangle(symbol: kExtensionUSR))
+        let modules = Set(root.breadthFirstSequence().filter { $0.kind == .module }.compactMap { $0.text })
+        XCTAssertEqual(modules, ["Unidirectional", "Onboarding", "LyftKit"])
+    }
+
     func testBreathFirstSequence() throws {
         let demangler = Demangler()
-        let root = try XCTUnwrap(demangler.demangle(symbol: Self.CommonSymbol))
+        let root = try XCTUnwrap(demangler.demangle(symbol: kCommonSymbol))
         let textNodes = root.breadthFirstSequence().compactMap { $0.text }
         XCTAssertEqual(textNodes, [
             "DriverCore",
